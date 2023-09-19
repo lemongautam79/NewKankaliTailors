b9a338a0035b9635201f8528c688a350470232c5
lemongautam79 <lemongautam79@gmail.com>
2023-09-19 22:01:47 +0545

    [Route]

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import { useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import ContactPage from './pages/ContactPage';
 
-export default function Routes() {
-    const routes = useRoutes([
-
-        //! Homepage 
-        {
-            path:'/',
-            element:<Homepage/>
-        },
-        {
-            path:'/contactus',
-            element:<ContactPage/>
-        }
-    ]);
-    return routes;
+const routes = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/contactus" element={<ContactPage />} />
+            </Routes>
+        </>
+    )
 }
+
+export default routes
