49fc606fa341ca324f55678eedf43aa173f441a7
lemongautam79 <lemongautam79@gmail.com>
2023-09-26 15:26:26 +0545

    [BaseURL]

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
-// export const BASE_URL = "https://new-kankali-tailors.onrender.com/";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://new-kankali-tailors.onrender.com/";
 export const WEBSOCKET_BASE_URL = "ws://192.168.1.70:8000/";
 // export const BASE_URL = "https://e4b5-2400-1a00-b050-1fd3-1898-9b8c-4792-925c.ngrok-free.app/"
